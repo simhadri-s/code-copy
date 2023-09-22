13abe8ed58d916fe0ab0fe4e367d36ff1c802006
simhadri <viveksimhadri89@gmail.com>
2023-09-22 22:28:06 +0530

    Cleared error:displaying the code in a single line

--- public/app.js
+++ public/app.js
@@ -60,65 +60,65 @@ function assisort() {
   for(let j=0; j<pgmnumarr.length; j++){
     darr.push({pgmnum: pgmnumarr[j], titleText: titlearr[j], contentText: codearr[j]});
   }
+  console.log(darr.contentText);
   darr.sort((a, b) => parseInt(a.pgmnum) - parseInt(b.pgmnum));
   crttble();
 }
 
 function crttble() {
   const tableBody = document.getElementById('tableBody');
-  const buttonText="Copy Code";
+  const buttonText = "Copy Code";
 
-    darr.forEach((contentObj, index) => {
-      const row = document.createElement('tr');
-      tableBody.appendChild(row);
+  darr.forEach((contentObj, index) => {
+    const row = document.createElement('tr');
+    tableBody.appendChild(row);
 
-      const slnoc = document.createElement('td');
-      const slno = document.createElement('t');
-      slno.textContent = contentObj.pgmnum;
-      slnoc.appendChild(slno);
-      console.log(slno);
-      row.appendChild(slnoc);
+    const slnoc = document.createElement('td');
+    const slno = document.createElement('t');
+    slno.textContent = contentObj.pgmnum;
+    slnoc.appendChild(slno);
+    row.appendChild(slnoc);
 
-      const titleCell = document.createElement('td');
-      const title = document.createElement('t');
-      title.textContent = contentObj.titleText;
-      titleCell.appendChild(title);
-      console.log(title);
-      row.appendChild(titleCell);
+    const titleCell = document.createElement('td');
+    const title = document.createElement('t');
+    title.textContent = contentObj.titleText;
+    titleCell.appendChild(title);
+    row.appendChild(titleCell);
 
-      const buttonCell = document.createElement('td');
-      const button = document.createElement('button');
-      button.textContent = buttonText;
-      buttonCell.appendChild(button);
-      row.appendChild(buttonCell);
+    const buttonCell = document.createElement('td');
+    const button = document.createElement('button');
+    button.textContent = buttonText;
+    buttonCell.appendChild(button);
+    row.appendChild(buttonCell);
 
-      const contentCell = document.createElement('td');
-      const paragraph = document.createElement('p');
-      paragraph.textContent = contentObj.contentText;
+    const contentCell = document.createElement('td');
+    const paragraph = document.createElement('p');
+    console.log(paragraph);
+    // Replace line breaks with <br> tags
+    paragraph.innerHTML = contentObj.contentText.replace(/\n/g, "<br>");
+    paragraph.style.display = 'none';
+    contentCell.appendChild(paragraph);
+    row.appendChild(contentCell);
+
+    button.addEventListener('click', () => {
+      paragraph.style.display = 'block';
+      const range = document.createRange();
+      range.selectNode(paragraph);
+
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand('copy');
+
+      selection.removeAllRanges();
       paragraph.style.display = 'none';
-      contentCell.appendChild(paragraph);
-      row.appendChild(contentCell);
 
-      button.addEventListener('click', () => {
-        paragraph.style.display = 'block';
-        const range = document.createRange();
-        range.selectNode(paragraph);
-
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-        document.execCommand('copy');
-
-        selection.removeAllRanges();
-        paragraph.style.display = 'none';
-
-        button.textContent = 'Copied!';
-        setTimeout(() => {
-          button.textContent = buttonText;
-        }, 2000);
-      });
+      button.textContent = 'Copied!';
+      setTimeout(() => {
+        button.textContent = buttonText;
+      }, 2000);
     });
-  
+  });
 }
 
 
